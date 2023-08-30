8fcdcd80e6a8c9b8e1b73ce28e061d747f6ee431
parameswarrout <parameswarrout73@gmail.com>
2023-08-30 18:00:36 +0530

    .\LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -5,7 +5,7 @@ class Node:
         
         
 class LinkedList:
-    def __init__(self):
+    def __init__(self,):
         self.head =None
         self.tail = None
         self.length = 0
@@ -30,12 +30,42 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
-    
-    
-    
+        
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail =  new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        
+    def insert(self, index, value):
+        new_node = Node(value)
+        if index < 0 or index > self.length:
+            return False
+        elif self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp_node = self.head
+            for _ in range(index-1):
+                temp_node = temp_node.next
+            new_node.next = temp_node.next
+            temp_node.next = new_node
+        self.length +=1
+        return True
+            
+
 new_linked_list = LinkedList()
 new_linked_list.append(10)
 new_linked_list.append(20)
 new_linked_list.append(30)
 new_linked_list.append(40)
+print(new_linked_list)
+new_linked_list.insert(3,25)
 print(new_linked_list)
